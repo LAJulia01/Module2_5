d9297cc63154bf6b61029b67ca9f55dd0edb7351
LAJulia01 <laurenceabjulia@gmail.com>
2024-10-01 14:50:16 +0800

    Oct 1 afternoon

--- lib/regpage.dart
+++ lib/regpage.dart
@@ -76,7 +76,7 @@
 //                 controller: _textFieldController,
 //                 labelText: 'Enter Text',
 //               ),
-              
+
 //               // Dropdown
 //               DropdownButton<String>(
 //                 value: dropdownValue,
@@ -94,7 +94,7 @@
 //                   });
 //                 },
 //               ),
-              
+
 //               // Radio Button
 //               RadioListTile<int>(
 //                 title: const Text('Option 1'),
@@ -116,7 +116,7 @@
 //                   });
 //                 },
 //               ),
-              
+
 //               // Checkbox
 //               CheckboxListTile(
 //                 title: const Text('Check me'),
@@ -127,21 +127,21 @@
 //                   });
 //                 },
 //               ),
-              
+
 //               // Date Picker
 //               ListTile(
 //                 title: Text('Select Date: ${selectedDate.toLocal()}'),
 //                 trailing: const Icon(Icons.calendar_today),
 //                 onTap: () => _selectDate(context),
 //               ),
-              
+
 //               // Time Picker
 //               ListTile(
 //                 title: Text('Select Time: ${selectedTime.format(context)}'),
 //                 trailing: const Icon(Icons.access_time),
 //                 onTap: () => _selectTime(context),
 //               ),
-              
+
 //               // Submit Button
 //               ElevatedButton(
 //                 onPressed: () {
@@ -238,7 +238,8 @@ class _RegisterPageState extends State<RegisterPage> {
     if (picked != null && picked != selectedDate) {
       setState(() {
         selectedDate = picked;
-        _birthDateController.text = "${picked.toLocal()}".split(' ')[0]; // Format date
+        _birthDateController.text =
+            "${picked.toLocal()}".split(' ')[0]; // Format date
       });
     }
   }
@@ -419,7 +420,8 @@ class _RegisterPageState extends State<RegisterPage> {
                 // Handle form submission
                 print("Gender: $dropdownGender");
                 print("Civil Status: $selectedStatus");
-                print("Subjects: Math: $isMathChecked, English: $isEnglishChecked, Science: $isScienceChecked");
+                print(
+                    "Subjects: Math: $isMathChecked, English: $isEnglishChecked, Science: $isScienceChecked");
                 print("Birthdate: ${_birthDateController.text}");
                 print("Start Time: ${_startTimeController.text}");
                 print("End Time: ${_endTimeController.text}");
@@ -427,6 +429,242 @@ class _RegisterPageState extends State<RegisterPage> {
               child: const Text('Submit'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+//
+
+class CustomRegister extends StatelessWidget {
+  const CustomRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CustomRegisterPage(),
+      debugShowCheckedModeBanner: false, // Hide the debug banner
+    );
+  }
+}
+
+class CustomRegisterPage extends StatefulWidget {
+  const CustomRegisterPage({super.key});
+
+  @override
+  _CustomRegisterPageState createState() => _CustomRegisterPageState();
+}
+
+class _CustomRegisterPageState extends State<CustomRegisterPage> {
+  // Controllers for text fields
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _birthDateController = TextEditingController();
+
+  // Dropdown Gender options
+  String dropdownGender = 'Male'; // Default gender
+
+  // Radio button Civil Status options
+  String selectedStatus = 'Single'; // Default civil status
+
+  // Form key for validation
+  final _formKey = GlobalKey<FormState>();
+
+  // Method to pick a date
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1950),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      setState(() {
+        _birthDateController.text =
+            "${picked.toLocal()}".split(' ')[0]; // Format date
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Custom Registration',
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepPurple, // App bar background color
+        centerTitle: true, // Center the title
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        child: Form(
+          key: _formKey, // Form key for validation
+          child: ListView(
+            children: [
+              const SizedBox(height: 20),
+
+              // Custom Username Field
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter your username',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  prefixIcon:
+                      const Icon(Icons.person, color: Colors.deepPurple),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Custom Password Field
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  prefixIcon: const Icon(Icons.lock, color: Colors.deepPurple),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Custom Dropdown for Gender
+              DropdownButtonFormField<String>(
+                value: dropdownGender,
+                decoration: InputDecoration(
+                  labelText: 'Gender',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  prefixIcon: const Icon(Icons.person_outline,
+                      color: Colors.deepPurple),
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownGender = newValue!;
+                  });
+                },
+                items: <String>['Male', 'Female', 'Other']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+
+              // Custom Radio buttons for Civil Status
+              const Text(
+                'Civil Status:',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurple),
+              ),
+              RadioListTile<String>(
+                title: const Text('Single'),
+                value: 'Single',
+                groupValue: selectedStatus,
+                activeColor: Colors.deepPurple,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedStatus = value!;
+                  });
+                },
+              ),
+              RadioListTile<String>(
+                title: const Text('Married'),
+                value: 'Married',
+                groupValue: selectedStatus,
+                activeColor: Colors.deepPurple,
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedStatus = value!;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Custom Date Picker for Birthdate
+              TextFormField(
+                controller: _birthDateController,
+                decoration: InputDecoration(
+                  labelText: 'Birthdate',
+                  hintText: 'Select your birthdate',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  suffixIcon: const Icon(Icons.calendar_today,
+                      color: Colors.deepPurple),
+                ),
+                readOnly: true,
+                onTap: () {
+                  _selectDate(context);
+                },
+              ),
+              const SizedBox(height: 40),
+
+              // Custom Submit Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor:
+                        Colors.deepPurple, // Button background color
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Handle form submission
+                      print("Username: ${_usernameController.text}");
+                      print("Password: ${_passwordController.text}");
+                      print("Gender: $dropdownGender");
+                      print("Civil Status: $selectedStatus");
+                      print("Birthdate: ${_birthDateController.text}");
+                    }
+                  },
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
